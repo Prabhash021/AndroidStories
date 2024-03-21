c0bddae7b5d823ae4465d820a333f667a7126d76
Prabhash021 <prabhash.shankar.21@gmail.com>
2024-03-21 21:02:42 +0530

    Added: - added the demo of the project in main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-//    implementation(project(":StoryView"))
-    /* implementation("com.github.Prabhash021:AndroidStories:0.0.1-beta")*/
+//    implementation(project(":StoryView")) // to add library locally in project
+     implementation("com.github.Prabhash021:AndroidStories:0.0.4-beta")
 }
